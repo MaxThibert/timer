7b2169f9009c65d64e3995fb40d68cb3bf8f54d3
MaxThibert <max.cdg@hotmail.fr>
2015-04-27 22:17:20 +0200

    timer

--- timer suite.py
+++ timer suite.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 27 21:55:04 2015
+Created on Mon Apr 27 21:55:15 2015
 
 @author: Maxime
 """
@@ -12,6 +12,9 @@ while True:
     if ev.type == QUIT: exit()
     elif ev.type == KEYDOWN:
         if not ev.key or ev.key== K_DOWN: print 'down'
+        elif ev.key == K_LEFT: print 'left'
+        elif ev.key == K_RIGHT: print 'right'
         elif ev.key == K_UP and not k_up_is_up:
             print 'up'
             k_up_is_up = True
+    elif ev.type == KEYUP and ev.key == K_UP: k_up_is_up = False
